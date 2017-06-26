967b0ecf6ba5338af5470abf4958350a5c492ea4
nswbmw <gxqzk@126.com>
2017-06-26 13:31:07 +0800

    add `error.message` column && bump v1.1.1

--- kibana_requestId_link.js
+++ kibana_requestId_link.js
@@ -33,7 +33,7 @@
       if (item.innerText.trim().match(/^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$/)) {
         var time = item.baseURI.match(/time\:\(.*?\)/)[0];
         var uuid = item.innerText;
-        var url = `http://kibana.shimo.run/app/kibana#/discover?_g=(refreshInterval:(display:Off,pause:!f,value:0),${time})&_a=(columns:!(step,take),index:'logstash-*',interval:auto,query:(query_string:(analyze_wildcard:!t,query:%22${uuid}%22)),sort:!(step,asc))`;
+        var url = `http://kibana.shimo.run/app/kibana#/discover?_g=(refreshInterval:(display:Off,pause:!f,value:0),${time})&_a=(columns:!(step,take,error.message),index:'logstash-*',interval:auto,query:(query_string:(analyze_wildcard:!t,query:%22${uuid}%22)),sort:!(step,asc))`;
         item.innerHTML = `<a href="${url}" target="_blank">${item.innerText}</a>`.replace('&amp;', '&');
       }
     });
